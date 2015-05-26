a61feb9d684a21fcf1f81bb94a3d5efdb167b5fe
Reza Shadman <pcfeeler@gmail.com>
2015-05-26 17:25:46 +0430

    Add arrable interface to make objects act like arrays

---
+++ ArrayableInterface.php
@@ -0,0 +1,12 @@
+<?php namespace Quince\Pelastic;
+
+interface ArrayableInterface {
+
+    /**
+     * An array representation of class
+     *
+     * @return array
+     */
+    public function toArray();
+
+}
