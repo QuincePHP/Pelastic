12b30f3190239b83e897175624808373e9c6ebbf
Reza Shadman <pcfeeler@gmail.com>
2015-05-26 22:41:16 +0430

    Add terms query contract

---
+++ src/Contracts/Queries/TermsQueryInterface.php
@@ -0,0 +1,39 @@
+<?php namespace Quince\Pelastic\Contracts\Queries;
+
+use Quince\Pelastic\Queries\TermsQuery;
+
+interface TermsQueryInterface {
+
+    /**
+     * Sets field to search on
+     *
+     * @param $field
+     * @return $this
+     */
+    public function setField($field);
+
+    /**
+     * A Proxy on set terms
+     *
+     * @param $terms
+     * @return TermsQuery
+     */
+    public function setIn($terms);
+
+    /**
+     * Acts like where in with exact values
+     *
+     * @param $terms
+     * @return $this
+     */
+    public function setTerms($terms);
+
+    /**
+     * A more human friendly proxy on set terms
+     *
+     * @param $terms
+     * @return $this
+     */
+    public function in($terms);
+
+}
