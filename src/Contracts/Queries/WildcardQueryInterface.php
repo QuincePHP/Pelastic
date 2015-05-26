70c550fb80742d4858c6c369329515abc20b1864
Reza Shadman <pcfeeler@gmail.com>
2015-05-26 23:08:41 +0430

    Add contracts for wild card query

---
+++ src/Contracts/Queries/WildcardQueryInterface.php
@@ -0,0 +1,63 @@
+<?php namespace Quince\Pelastic\Contracts\Queries;
+
+interface WildcardQueryInterface {
+
+    /**
+     * Set field to apply wildcard query
+     *
+     * @param $field
+     * @return $this
+     */
+    public function setWildCardField($field);
+
+    /**
+     * A proxy on setWildCardFieldMethod
+     *
+     * @param $field
+     * @return $this
+     */
+    public function setField($field);
+
+    /**
+     * A proxy on set field method
+     *
+     * @param $field
+     * @return $this
+     */
+    public function forField($field);
+
+    /**
+     * Set wild card value
+     *
+     * @param $value
+     * @return $this
+     */
+    public function setWildCardValue($value);
+
+    /**
+     * A proxy on setWildCardValue
+     *
+     * @param $value
+     * @return $this
+     */
+    public function like($value);
+
+    /**
+     * Create string for wildcard matching with "?"
+     *
+     * @param string $first
+     * @param string $second
+     * @return string
+     */
+    public function createSingleAny($first = '', $second = '');
+
+    /**
+     * Create string for wildcard matching with "*"
+     *
+     * @param string $first
+     * @param string $second
+     * @return string
+     */
+    public function createMultiAny($first = '', $second = '');
+
+}
