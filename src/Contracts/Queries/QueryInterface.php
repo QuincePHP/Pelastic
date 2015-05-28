63c715c21f81992bd7efbe99631e93211f4e1b36
Reza Shadman <pcfeeler@gmail.com>
2015-05-28 10:43:27 +0430

    Fix namespace

--- src/Contracts/Queries/QueryInterface.php
+++ src/Contracts/Queries/QueryInterface.php
@@ -1,5 +1,6 @@
 <?php namespace Quince\Pelastic\Contracts\Queries;
 
+use Quince\Pelastic\Contracts\AccessorMutatorInterface;
 use Quince\Pelastic\Contracts\ArrayableInterface;
 
 interface QueryInterface extends AccessorMutatorInterface, BoostableInterface, ArrayableInterface {
