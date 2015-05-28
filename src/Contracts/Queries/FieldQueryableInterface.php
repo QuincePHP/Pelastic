fde119092cd177c5298a94c8060ffa05acf64fb9
Reza Shadman <pcfeeler@gmail.com>
2015-05-28 10:25:59 +0430

    Add a queryable contract

---
+++ src/Contracts/Queries/FieldQueryableInterface.php
@@ -0,0 +1,29 @@
+<?php namespace Quince\Pelastic\Contracts\Queries;
+
+interface FieldQueryableInterface {
+
+    /**
+     * Set field to exact match a value inside that
+     *
+     * @param string $field
+     * @return TermQueryInterface
+     */
+    public function setField($field);
+
+    /**
+     * Set value to exact match against the field values
+     *
+     * @param string $value
+     * @return $this
+     */
+    public function setValue($value);
+
+    /**
+     * A proxy on set value
+     *
+     * @param $query
+     * @return $this
+     */
+    public function setQuery($query);
+
+}
