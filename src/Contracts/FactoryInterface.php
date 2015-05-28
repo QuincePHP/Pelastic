e83910de45b4a69adc52f393201bb33b2b191190
Reza Shadman <pcfeeler@gmail.com>
2015-05-28 10:24:27 +0430

    Add a factory contract for all factory classes

---
+++ src/Contracts/FactoryInterface.php
@@ -0,0 +1,25 @@
+<?php namespace Quince\Pelastic\Contracts;
+
+use Quince\Pelastic\Contracts\Queries\QueryInterface;
+
+interface FactoryInterface {
+
+    /**
+     * Creates the factory object of the given class with given arguments
+     *
+     * @param string $class
+     * @param array  $args
+     * @return QueryInterface
+     */
+    public function createFromClass($class, array $args = []);
+
+    /**
+     * Creates factory objects from keywords
+     *
+     * @param string $what
+     * @param array  $args
+     * @return QueryInterface
+     */
+    public function create($what, array $args = []);
+
+}
