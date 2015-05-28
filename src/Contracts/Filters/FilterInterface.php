d6ea3607d3ade31960c23396d186c17455c030dc
Reza Shadman <pcfeeler@gmail.com>
2015-05-28 10:29:49 +0430

    Add accessor mutator contract to filter interface

--- src/Contracts/Filters/FilterInterface.php
+++ src/Contracts/Filters/FilterInterface.php
@@ -1,7 +1,8 @@
 <?php namespace Quince\Pelastic\Contracts\Filters;
 
 use Quince\Pelastic\Contracts\ArrayableInterface;
+use Quince\Pelastic\Contracts\Queries\AccessorMutatorInterface;
 
-interface FilterInterface extends ArrayableInterface {
+interface FilterInterface extends ArrayableInterface, AccessorMutatorInterface{
 
 }
