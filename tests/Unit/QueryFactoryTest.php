9911515dc1dc030057a8f8b7b15ebeb5f1720d8e
Reza Shadman <pcfeeler@gmail.com>
2015-05-28 21:52:21 +0430

    TWest query factory class

--- tests/Unit/QueryFactoryTest.php
+++ tests/Unit/QueryFactoryTest.php
@@ -1,8 +1,11 @@
 <?php namespace Quince\Pelastic\Tests\Unit;
 
 use Quince\Pelastic\Contracts\FactoryInterface;
+use Quince\Pelastic\Contracts\Queries\QueryInterface;
 use Quince\Pelastic\Contracts\QueryFactoryInterface;
 use Quince\Pelastic\Exceptions\PelasticInvalidArgumentException;
+use Quince\Pelastic\Exceptions\PelasticLogicException;
+use Quince\Pelastic\Queries\TermQuery;
 use Quince\Pelastic\QueryFactory;
 
 class QueryFactoryTest extends BaseTestCase {
@@ -30,9 +33,40 @@ class QueryFactoryTest extends BaseTestCase {
 
     public function test_it_can_create_from_class()
     {
+        $termQuery = $this->mainObject->createFromClass(TermQuery::class);
+
+        $this->assertTrue(is_a($termQuery, TermQuery::class));
+
         $this->setExpectedException(PelasticInvalidArgumentException::class);
 
         $this->mainObject->createFromClass('\StdClass');
     }
 
+    public function test_it_can_create_from_keyword()
+    {
+        $termQuery = $this->mainObject->create('term');
+
+        $this->assertTrue(is_a($termQuery, TermQuery::class));
+
+        $this->setExpectedException(PelasticLogicException::class);
+
+        $termQuery->toArray();
+
+        $termQuery = $this->mainObject->create('term', ['title', 'some title']);
+
+        $expected = [
+            'term' => [
+                'title' => [
+                    'value' => 'some title'
+                ]
+            ]
+        ];
+
+        $this->assertEquals($expected, $termQuery->toArray());
+    }
+
+    public function test_get_interface()
+    {
+        $this->assertEquals($this->mainObject->getInterface(), QueryInterface::class);
+    }
 }
