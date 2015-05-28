89c8d8d56d7b5496b49eabb8bd39b68cb6ce4a41
Reza Shadman <pcfeeler@gmail.com>
2015-05-28 10:24:57 +0430

    Add static factory contract

---
+++ src/Contracts/StaticFactoryInterface.php
@@ -0,0 +1,25 @@
+<?php namespace Quince\Pelastic\Contracts;
+
+use Quince\Pelastic\Contracts\Queries\QueryInterface;
+
+interface StaticFactoryInterface {
+
+    /**
+     * Creates the factory object of the given class with given arguments
+     *
+     * @param string $class
+     * @param array  $args
+     * @return QueryInterface
+     */
+    public static function createFromClass($class, array $args = []);
+
+    /**
+     * Creates factory objects from keywords
+     *
+     * @param string $what
+     * @param array  $args
+     * @return QueryInterface
+     */
+    public static function create($what, array $args = []);
+
+}
