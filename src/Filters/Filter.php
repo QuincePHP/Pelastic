b176684221efe08324eb20eb004278ea8728b550
Reza Shadman <pcfeeler@gmail.com>
2015-05-29 08:21:51 +0430

    Add extracted method of accessor and mutator by using the trait

--- src/Filters/Filter.php
+++ src/Filters/Filter.php
@@ -1,9 +1,10 @@
 <?php namespace Quince\Pelastic\Filters;
 
+use Quince\Pelastic\AccessibleMutatableTrait;
 use Quince\Pelastic\Contracts\Filters\FilterInterface;
 
 abstract class Filter implements FilterInterface {
 
-    
+    use AccessibleMutatableTrait;
 
 }
