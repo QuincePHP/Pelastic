4eece7cab8357b73508fca9d36b31c24313f84f9
Behzad Shabani <behzad.shabani@gmail.com>
2015-05-29 12:01:22 +0430

    Fix error in use statements

--- src/Queries/FilteredQuery.php
+++ src/Queries/FilteredQuery.php
@@ -1,6 +1,6 @@
 <?php namespace Quince\Pelastic\Queries;
 
-use Quince\Exceptions\PelasticInvalidArgumentException;
+use Quince\Pelastic\Exceptions\PelasticInvalidArgumentException;
 use Quince\Pelastic\Contracts\Filters\FilterInterface;
 use Quince\Pelastic\Contracts\Queries\FilteredQueryInterface;
 use Quince\Pelastic\Contracts\Queries\QueryInterface;
