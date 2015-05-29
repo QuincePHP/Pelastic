aadaa8fe3368922bc18b7e96f68903c2d9f92fd4
Reza Shadman <pcfeeler@gmail.com>
2015-05-29 08:49:51 +0430

    Add a method for adding a filter to the optiona container in Base Filter class.

--- src/Filters/Filter.php
+++ src/Filters/Filter.php
@@ -7,4 +7,16 @@ abstract class Filter implements FilterInterface {
 
     use AccessibleMutatableTrait;
 
+    /**
+     * Put a filter interface instance into array fields
+     *
+     * @param string          $field
+     * @param FilterInterface $filter
+     * @return array
+     */
+    protected function putQueryIntoArrayField($field, FilterInterface $filter)
+    {
+        return $this->putIntoArrayField($field, $filter);
+    }
+
 }
