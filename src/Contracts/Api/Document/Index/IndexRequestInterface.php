7845693b0c91c429a0e595805fdd894756015893
Reza Shadman <pcfeeler@gmail.com>
2015-06-05 14:41:14 +0430

    Create index request for a document(contract).

--- src/Contracts/Api/Document/Index/IndexRequestInterface.php
+++ src/Contracts/Api/Document/Index/IndexRequestInterface.php
@@ -1,7 +1,25 @@
 <?php namespace Quince\Pelastic\Contracts\Api\Document\Index;
 
 use Quince\Pelastic\Contracts\Api\RequestInterface;
+use Quince\Pelastic\Contracts\DocumentInterface;
 
 interface IndexRequestInterface extends  RequestInterface {
 
+    /**
+     * Set document to work on
+     *
+     * @param DocumentInterface $document
+     * @param null $id
+     * @return $this
+     */
+    public function setDocument(DocumentInterface $document, $id = null);
+
+    /**
+     * Set id
+     *
+     * @param null $id
+     * @return $this
+     */
+    public function setId($id = null);
+    
 }
