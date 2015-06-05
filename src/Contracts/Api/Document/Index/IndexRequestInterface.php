09674f1f898f1472fd31e67a475a5ddd01fa579a
Reza Shadman <pcfeeler@gmail.com>
2015-06-05 14:26:27 +0430

    Add contract for the request for indexing a document

---
+++ src/Contracts/Api/Document/Index/IndexRequestInterface.php
@@ -0,0 +1,7 @@
+<?php namespace Quince\Pelastic\Contracts\Api\Document\Index;
+
+use Quince\Pelastic\Contracts\Api\RequestInterface;
+
+interface IndexRequestInterface extends  RequestInterface {
+
+}
