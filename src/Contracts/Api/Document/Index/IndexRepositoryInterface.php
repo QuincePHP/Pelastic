549fe6d9f71da3cf4f61e4c1f6b752fac74da9b0
Reza Shadman <pcfeeler@gmail.com>
2015-06-05 14:26:52 +0430

    Add repository contract for executing an index request against the elasticsearch related endpoint.

---
+++ src/Contracts/Api/Document/Index/IndexRepositoryInterface.php
@@ -0,0 +1,7 @@
+<?php namespace Quince\Pelastic\Contracts\Api\Document\Index;
+
+use Quince\Pelastic\Contracts\Api\RepositoryInterface;
+
+interface IndexRepositoryInterface extends RepositoryInterface {
+
+}
