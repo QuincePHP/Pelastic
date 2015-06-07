9c51e0d6fef4a4124dc98de60d2b7fee2ce9e2a0
Reza Shadman <pcfeeler@gmail.com>
2015-06-07 22:52:22 +0430

    Fix Indexrequest contract bugs.

--- src/Contracts/Api/Document/Index/IndexRequestInterface.php
+++ src/Contracts/Api/Document/Index/IndexRequestInterface.php
@@ -17,9 +17,63 @@ interface IndexRequestInterface extends  RequestInterface {
     /**
      * Set id
      *
-     * @param null $id
+     * @param $id
      * @return $this
      */
-    public function setId($id = null);
-    
+    public function setId($id);
+
+    /**
+     * Index to store document on
+     *
+     * @param $index
+     * @return $this
+     */
+    public function setIndex($index);
+
+    /**
+     * Type to set document on
+     *
+     * @param $type
+     * @return $this
+     */
+    public function setType($type);
+
+    /**
+     * Index with all args
+     *
+     * @param DocumentInterface $document
+     * @param $index
+     * @param null $type
+     * @param null $id
+     * @return mixed
+     */
+    public function index(DocumentInterface $document, $index, $type = null, $id = null);
+
+    /**
+     * Get document of the request
+     *
+     * @return DocumentInterface
+     */
+    public function getDocument();
+
+    /**
+     * Get id of the document
+     *
+     * @return string|integer
+     */
+    public function getDocumentId();
+
+    /**
+     * Get index that should be used to store documents on.
+     *
+     * @return string
+     */
+    public function getIndex();
+
+    /**
+     * Get type that should be used to store documents on.
+     *
+     * @return string
+     */
+    public function getType();
 }
