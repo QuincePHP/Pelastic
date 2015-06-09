55ba7e777395f2e4c40f95ff7932f06eca278d97
Reza Shadman <pcfeeler@gmail.com>
2015-06-09 12:34:42 +0430

    Create http connection interface contract.

---
+++ src/Contracts/Connection/HttpConnectionInterface.php
@@ -0,0 +1,17 @@
+<?php namespace Illuminate\Contracts\Connection;
+
+interface HttpConnectionInterface {
+
+    /**
+     * Perform an http request
+     *
+     * @param $method
+     * @param $uri
+     * @param array $params
+     * @param array $body
+     * @param array $options
+     * @return mixed
+     */
+    public function performRequest($method, $uri, array $params = null, array $body = null, array $options = null);
+
+}
