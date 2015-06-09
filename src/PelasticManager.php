810dd197eab775aabab6c49c50ffdbdc85dca1ac
Reza Shadman <pcfeeler@gmail.com>
2015-06-09 14:12:06 +0430

    Add QueryBuilder instantiator to the Pelastic manager .

--- src/PelasticManager.php
+++ src/PelasticManager.php
@@ -5,40 +5,35 @@ use Illuminate\Contracts\Connection\Pool\ConnectionPoolInterface;
 use Quince\Pelastic\Connection\Selector\PerRequestRoundRobinSelector;
 use Quince\Pelastic\Exceptions\PelasticInvalidArgumentException;
 use Quince\Pelastic\Exceptions\PelasticLogicException;
+use Quince\Pelastic\Filters\BooleanFilter;
+use Quince\Pelastic\Queries\BooleanQuery;
+use Quince\Pelastic\SqlMonkey\QueryBuilder;
 
 class PelasticManager {
 
     /**
-     * Global config of the manager
+     * Global config of the manager which is given by user, things like hosts
+     * needed selector strategies and son
      *
      * @var PelasticConfigInterface
      */
     protected static $config;
 
     /**
+     * This class is like a facade or a proxy to other features of Pelastic
+     * as we haven't used any dependency container strategy because of its performance
+     * overhead.
+     *
      * @var PelasticManager
      */
     protected static $instance;
 
     /**
+     * The global connection pool all of the connection instance are provided by this pool
+     *
      * @var ConnectionPoolInterface
      */
     protected $pool;
-
-    /**
-     * Prevent un-serialize
-     */
-    protected function __wakeup() {}
-
-    /**
-     * Prevent cloning
-     */
-    protected function __clone() {}
-
-    /**
-     * Prevent new 
-     */
-    protected function __construct() {}
 
     /**
      * Get singleton instance of the class
@@ -52,8 +47,11 @@ class PelasticManager {
         }
 
         if (null === (static::$instance)) {
+
+            // We simply bootstrap our needed dependencies after instantiating the object
             static::$instance = new static();
             static::$instance->bootstrap();
+
         }
 
         return static::$instance;
@@ -64,12 +62,23 @@ class PelasticManager {
      */
     protected function bootstrap()
     {
+        // The config provided by the user contains of two important parts
+        // firs a Selector "object" which is called by connection pool
+        // to select a connection from an array of collections with different strategies
+        // like round robin
         $class = $this->getConfig()->getConnectionPoolStrategy();
 
-        $this->pool = new $class($this->getSelector());
+        if (!is_object($class)) {
+            $this->pool = new $class($this->getSelector());
+        }else {
+            $this->pool = $class;
+        }
 
+        // We simply make sure that the given pool is from our interface
         if (!is_a($this->pool, $interface = ConnectionPoolInterface::class)) {
-            throw new PelasticInvalidArgumentException("You have selected an invalid connection pool all pools should implement the [$interface] interface.");
+            throw new PelasticInvalidArgumentException(
+                "You have selected an invalid connection pool all pools should implement the [$interface] interface."
+            );
         }
     }
 
@@ -101,6 +110,7 @@ class PelasticManager {
      */
     public function getSelector()
     {
+        // If we have not set the selector in config section we will set it now.
         if (null === $selector = $this->getConfig()->getSelector()) {
             $selector = $this->getConfig()->setSelector(new PerRequestRoundRobinSelector());
         }
@@ -117,4 +127,33 @@ class PelasticManager {
     {
         return $this->pool;
     }
+
+    /**
+     * Get sql monkey
+     *
+     * @return QueryBuilder
+     */
+    public function getSqlMonkey()
+    {
+        return new QueryBuilder(
+            $this,
+            new BooleanFilter(),
+            new BooleanQuery()
+        );
+    }
+
+    /**
+     * Prevent un-serialize
+     */
+    protected function __wakeup() {}
+
+    /**
+     * Prevent cloning
+     */
+    protected function __clone() {}
+
+    /**
+     * Prevent new
+     */
+    protected function __construct() {}
 }
