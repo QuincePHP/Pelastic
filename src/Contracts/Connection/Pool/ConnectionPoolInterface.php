ba0227b27d9fdad0d2c0033a92018f6d27f22697
Reza Shadman <pcfeeler@gmail.com>
2015-06-09 12:58:12 +0430

    Add method to connection pool interface.

--- src/Contracts/Connection/Pool/ConnectionPoolInterface.php
+++ src/Contracts/Connection/Pool/ConnectionPoolInterface.php
@@ -1,5 +1,16 @@
 <?php namespace Illuminate\Contracts\Connection\Pool;
 
+use Illuminate\Contracts\Connection\HttpConnectionInterface;
+use Quince\Pelastic\Exceptions\PelasticException;
+
 interface ConnectionPoolInterface {
+
+    /**
+     * Get next connection from the pool
+     *
+     * @return HttpConnectionInterface
+     * @throws PelasticException
+     */
+    public function getNextConnection();
 
 }
