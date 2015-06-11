08b0bd3ee897ac6f1e370410c897fb9131159240
Reza Shadman <pcfeeler@gmail.com>
2015-06-11 17:35:15 +0430

    Fix namespace.

--- src/Api/Request/Request.php
+++ src/Api/Request/Request.php
@@ -1,22 +1,11 @@
-<?php namespace Quince\Pelastic\Api;
+<?php namespace Quince\Pelastic\Api\Request;
 
-
-use Elasticsearch\Client;
-use Quince\Pelastic\Contracts\Api\Request\Http;
 use Quince\Pelastic\Contracts\Api\Request\RequestInterface;
-use Quince\Pelastic\Contracts\Api\Request\ResponseInterface;
 use Quince\Pelastic\Utils\AccessibleMutatableTrait;
 
 abstract class Request implements RequestInterface {
 
     use AccessibleMutatableTrait;
-
-    /**
-     * Convert a request to http
-     *
-     * @return Http
-     */
-    abstract public function toHttp();
 
     /**
      * Convert request to an elasticsearch client representation
