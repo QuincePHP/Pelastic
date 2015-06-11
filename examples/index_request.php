e7f0287b390820182c33b81531021f567c2f9c89
Reza Shadman <pcfeeler@gmail.com>
2015-06-11 18:50:11 +0430

    Add more details.

--- examples/index_request.php
+++ examples/index_request.php
@@ -2,18 +2,50 @@
 
 require_once __DIR__ . '/bootstrap.php';
 
+use Elasticsearch\ClientBuilder;
+use Quince\Pelastic\PelasticManager;
+use Quince\Pelastic\Document;
+use Quince\Pelastic\Api\Endpoints\Document\Index\IndexRequest;
+
+/*
+ * Create the global elasticsearch client builder
+ */
 /** @var \Elasticsearch\Client $client */
-$client = \Elasticsearch\ClientBuilder::create()->setHosts(['http://localhost:9200'])->build();
+$client = ClientBuilder::create()->setHosts(['http://localhost:9200'])->build();
 
-$manager = new \Quince\Pelastic\PelasticManager($client);
+/*
+ * Create a new PelasticManager Instance
+ * Then pass the elasticsearch client to that
+ */
+$manager = new PelasticManager($client);
 
-$document = new \Quince\Pelastic\Document(['name' => 'reza', 'last_name' => 'shadman'], time());
+$document = new Document(['name' => 'reza', 'last_name' => 'shadman']);
 
-$indexRequest = new \Quince\Pelastic\Api\Endpoints\Document\Index\IndexRequest();
+$indexRequest = (new IndexRequest())->setIndex('users')
+                                    ->setType('common')
+                                    ->setDocument($document);
 
-$indexRequest->setIndex('users')->setType('common')->setDocument($document);
-
+/*
+ * When you execute a request, the above document is updated (the response id is injected to that)
+ * The IndexResponseInterface class is a data object containing response attributes
+ */
 /** @var \Quince\Pelastic\Contracts\Api\Endpoints\Document\Index\IndexResponseInterface $result */
 $result = $manager->executeRequest($indexRequest);
 
-var_dump($result->id());
+/*
+ * This is the raw result data
+ */
+echo "\n Raw Result Data:\n";
+var_dump($result->toArray());
+
+/*
+ * This is the updated/generated document id
+ */
+echo "\nDocument ID:\n";
+var_dump($document->getId());
+
+/*
+ * This is the version of the document
+ */
+echo "\nDocument Version:\n";
+var_dump($result->getVersion());
