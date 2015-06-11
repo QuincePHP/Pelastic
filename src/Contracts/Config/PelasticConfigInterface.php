80709de8360a0ea1cb96ae34bbd9a0d4ef9f73a8
Reza Shadman <pcfeeler@gmail.com>
2015-06-11 12:41:25 +0430

    New strategy for pelastic config interface.

--- src/Contracts/Config/PelasticConfigInterface.php
+++ src/Contracts/Config/PelasticConfigInterface.php
@@ -3,70 +3,11 @@
 interface PelasticConfigInterface {
 
     /**
-     * Set hosts to be used to init connections
+     * Bootstrap from array
      *
-     * @param array $hosts
+     * @param array $params
      * @return $this
      */
-    public function setHosts(array $hosts);
+    public function fromArray(array $params);
 
-    /**
-     * Set auth params
-     *
-     * @param $username
-     * @param $password
-     * @param string $method
-     * @return $this
-     */
-    public function setAuth($username, $password, $method = 'Basic');
-
-    /**
-     * Get array of configs
-     *
-     * @return array
-     */
-    public function getArray();
-
-    /**
-     * Get hosts
-     *
-     * @return array
-     */
-    public function getHosts();
-
-    /**
-     * Does config uses a basic http auth?
-     *
-     * @return bool
-     */
-    public function usesHttpAuth();
-
-    /**
-     * Get auth params
-     *
-     * @return array
-     */
-    public function getAuthParams();
-
-    /**
-     * Selector instance which is used to select a connection in a pool of connections
-     *
-     * @param $selector
-     * @return $this
-     */
-    public function setSelector($selector);
-
-    /**
-     * Get used selector
-     *
-     * @return mixed
-     */
-    public function getSelector();
-
-    /**
-     * Gets the name the class which is used as the connection pool instance for the pelastic manager
-     *
-     * @return mixed
-     */
-    public function getConnectionPoolStrategy();
 }
