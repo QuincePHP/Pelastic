d8e749149002588156cc2c65617631755eee88f6
Reza Shadman <pcfeeler@gmail.com>
2015-06-12 15:00:20 +0430

    Better example.

--- examples/get_request.php
+++ examples/get_request.php
@@ -15,16 +15,23 @@ $manager = new PelasticManager($client);
 $indexRequest = new IndexRequest();
 $indexRequest->setIndex('users')
              ->setType('common')
-             ->setDocument($document = new Document(['username' => 'John'], 'document1'));
+             ->setDocument($document = new Document([
+                 'username' => 'pretty_john',
+                 'name' => 'John Snow',
+                 'nationality' => 'Winterfell'
+             ], 'persons1'));
 
 $result = $manager->executeRequest($indexRequest);
 $documentId = $document->getId();
 
 
-$getRequest = (new GetRequest())->setId($documentId)->setIndex('users')->setType('common');
+$getRequest = (new GetRequest())->setId($documentId)
+                                ->setIndex('users')
+                                ->setType('common')
+                                ->setSource(['username', 'name']);
 
 /** @var \Quince\Pelastic\Contracts\Api\Endpoints\Document\Get\GetResponseInterface $result */
 $result = $manager->executeRequest($getRequest);
 
-var_dump($result->getDocument()->toArray());
+var_dump($result->getDocument()->toArray(true));
 var_dump($result->toArray());
