1902dfde518fbf5f7f0fdd04f73e1b0ce78b930f
Reza Shadman <pcfeeler@gmail.com>
2015-06-14 16:53:00 +0430

    Add new exception for mass assignment.

---
+++ src/Exceptions/PelasticMassAssignmentException.php
@@ -0,0 +1,7 @@
+<?php namespace Quince\Pelastic\Exceptions;
+
+use Quince\Pelastic\Contracts\Exceptions\PelasticExceptionInterface;
+
+class PelasticMassAssignmentException extends PelasticInvalidArgumentException implements PelasticExceptionInterface {
+
+}
